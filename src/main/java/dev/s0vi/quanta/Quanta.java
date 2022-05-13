ccab48947c2c23767911a74c2cf9d78c72a82d88
clare <clara@DESKTOP-SVLILSG>
2022-05-12 20:16:19 -0500

    De-quilting

---
+++ src/main/java/dev/s0vi/quanta/Quanta.java
@@ -0,0 +1,11 @@
+package dev.s0vi.quanta;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class Quanta implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
